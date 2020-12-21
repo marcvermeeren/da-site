3542e02611e025ae203f4196b146b3e28468634e
marcvermeeren <m@marcvermeeren.com>
2020-12-21 17:42:42 +0100

    canvas embed

---
+++ _site/src/js/app.js
@@ -0,0 +1,9 @@
+initializeCollisions({
+    canvas: document.querySelector('#artwork'),
+    backgroundColor: '#110411',
+    strokeColor: '#b4ab8b',
+    collisionColor: '#e2cdaa',
+    burstColor: '#068af5',
+    burstSize: 4,
+    burstThickness: 1,
+  });
--- content/index.md
+++ content/index.md
@@ -2,4 +2,3 @@
 layout: page-iframe-home.njk
 title: Decentralized Agency
 ---
-<iframe seamless src="https://gracious-nightingale.netlify.app/">Your browser doesn't support iframes.</iframe>
